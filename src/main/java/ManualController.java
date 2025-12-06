d2ef890ebfd0fcb9c0e07113be2d631c3263ff11
chloejosien <chloejosien@gmail.com>
2025-12-06 11:56:19 -0800

    layout for Beehive qualifier

--- src/main/java/ManualController.java
+++ src/main/java/ManualController.java
@@ -9,11 +9,17 @@ users will then need to update the file path that they want to use.
 
 public class ManualController {
     //easy update for offline workings
+    //Beehive - Decode
     private static final String rankFilePath = "/Beehive-Decode/beehiveRank.txt";
-    private static final String allianceFilePath= "Beehive-Decode/beehiveAlliances.txt";
-    private static final String eliminationOrderFilePath= "Beehive-Decode/beehiveElim.txt";
-    private static final String awardFilePath = "Beehive-Decode/beehiveAwards.txt";
+    private static final String allianceFilePath= "/Beehive-Decode/beehiveAlliances.txt";
+    private static final String eliminationOrderFilePath= "/Beehive-Decode/beehiveElim.txt";
+    private static final String awardFilePath = "/Beehive-Decode/beehiveAwards.txt";
 
+    //Utah Champs- into the deep
+//    private static final String rankFilePath = "/rank.txt";
+//    private static final String allianceFilePath= "/alliances.txt";
+//    private static final String eliminationOrderFilePath= "/eliminationOrder.txt";
+//    private static final String awardFilePath = "/awards.txt";
     public static void main(String[] args) {
         AdvancementService advancementCalculator = new AdvancementService();
 
